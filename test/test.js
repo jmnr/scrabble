913fe3842e1a6950691f8cc40c2d52bea7238d89
Nikhila Ravi <nikkiravi92@gmail.com>
2015-05-28 17:02:24 +0100

    changed tests 100% coverage

--- test/test.js
+++ test/test.js
@@ -7,32 +7,30 @@ var ac = require('../index.js');
 assert.equal(typeof ac, 'object');
 assert.equal(typeof ac.import, 'function');
 
-// console.log("# ac.words imports a list of words into memory");
-// ac.import(function (err, words) {
-//   console.log("words.txt had " + words.length + " words");
-//   assert.equal(words.length, 99157);
-// });
+console.log("# ac.import imports a list of words into memory");
+ac.import(function (err, words) {
+  console.log("words.txt had " + words.length + " words");
+  assert.equal(words.length, 178691);
+});
 
 console.log("# attempt to invoke ac.import without callback");
 var error = ac.import('string');
 assert.equal(error.message, "callback argument MUST be a function");
 
-// console.log("# ac.findWord finds a string in a words array");
-// ac.import(function() {
-//   console.log('one');
-//   ac.findWord('Abe', function (err, found) {
-//     assert.equal(err, null);
-//     assert.equal(found.length, 3);
-//   });
-// });
-//
+console.log("# ac.numWords returns the correct word");
+ac.import(function() {
+  var scrab = 'abul__';
+  var result = ac.numWords(scrab.length, scrab).join(',');
+  assert.equal(result, "abulia,abulic");
+});
+
 // console.log("# ac.stats keeps a timestamp from when searched");
 // ac.import(function() {
 //   ac.stats('Abe', function (err, found) {
 //   });
 // });
 
-// console.log("# ac.findWords attempts to find a string in words array");
+// console.log("# ac.findWord attempts to find a string in words array");
 // ac.import(function() {
 //   ac.findWord('Ac', function (err, found) {
 //     assert.equal(err, null);
@@ -40,13 +38,12 @@ assert.equal(error.message, "callback argument MUST be a function");
 //   });
 // });
 
-// console.log('# ac.stats tracks which words/string were searched for');
-// ac.import(function () {
-//       ac.stats('awesome', function (err, stats) {
-//       // console.log(stats);
-//       assert.equal(stats.awesome.length, 2);
-//       console.log("ok " + testindex++ + " stats (search count) for awesome: ", stats.awesome.length);
-//
-//     });
-//   });
-// });
+console.log("# ac.match parses array correctly");
+var scrab = 'd__';
+var arr = ['hat', 'dog', 'cat'];
+assert.equal(ac.match(arr, scrab), 'dog');
+
+console.log("# ac.indexes finds match to word")
+var scrab = 'd__';
+var arr = ['hat', 'dog', 'cat'];
+assert.equal(ac.indexes(arr, 'd', 0), 'dog');
