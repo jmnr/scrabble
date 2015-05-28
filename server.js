058fab05dee6f4804421b3a309d24ec961aac31e
wallcrawler <ronan_mccabe@hotmail.com>
2015-05-28 14:48:18 +0100

    merged with josh

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var ac = require('./index.js');
 ac.import(function(err, count) {
   console.log("Imported " + count.length + " Scrabble words!");
 });
-// ok1
+// update
 var fs = require('fs');
 var index = fs.readFileSync(__dirname + '/index.html');
 
@@ -14,7 +14,7 @@ http.createServer(function handler(request, response) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(index.toString());
   }
-  if (url.length > 1) { //if find is in the url
+  if (url.length > 1) {
     var scrab = url.split('/')[1];
     var ok = ac.numWords(scrab.length, scrab).join(',');
     if(ok.length === 0) {response.end("No results found!")}
@@ -25,27 +25,3 @@ http.createServer(function handler(request, response) {
 }).listen(port);
 
 console.log('node http server listening on http://localhost:' + port);
-
-//http from nelson
-// http.createServer(function handler(request, response) {
-//   //console.log(request);
-//   var url = request.url;
-//   console.log("request.url:", url);
-//   if (url.length === 1) {
-//     response.writeHead(200, {"Content-Type": "text/html"});
-//     response.end(index.toString());
-//   }
-//   if (url.indexOf('/find/') > -1) { //if find is in the url
-//     // localhost:3000/find/word
-//     var word = url.split('/')[2];
-//     console.log(word);
-//     ac.findWord(word, function (err, found){
-//       // console.log(found);
-//       response.end(found.join(','));
-//     });
-//     // response.end('word: ', word);
-//   }
-//   else {
-//     response.end('hello Dan!');
-//   }
-// }).listen(port);
