6e052385c5ca76aecae9a901a53cbf3eba9e0e8c
wallcrawler <ronan_mccabe@hotmail.com>
2015-05-28 15:04:13 +0100

    overwriting master

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ var ac = {};
 
 ac.import = function (callback) {
   if (!callback || typeof callback !== 'function') {
-    return new Error('callback argument MUST be a function')
+    return new Error('callback argument MUST be a function');
   }
   var filename = __dirname + '/words.txt';
   fs.readFile(filename, 'utf8', function (err, data) {
     ac.words = data.split('\n');
     return callback(err, ac.words);
   });
-}
+};
 
 ac.stats = function (word, callback) {
   if (!ac.searches) {
@@ -21,7 +21,7 @@ ac.stats = function (word, callback) {
   }
   ac.searches[word].push(new Date().getTime());
   callback(null, ac.searches);
-}
+};
 
 ac.findWord = function (word, callback) {
   // who wants to volunteer to implement the method?
@@ -31,9 +31,32 @@ ac.findWord = function (word, callback) {
       found.push(ac.words[i]);
     }
   }
+  console.log('found.length', found.length);
   return callback(null, found);
-}
+};
 
+ac.numWords = function (num, scrab) { //will be hardcoded as 6 for time being
+  // console.log('indexes', ac.words[7]);
+  var words = ac.words.filter(function(x) {
+    return x.length === num;
+  });
+  return ac.match(words, scrab);
+};
 
+ac.match = function (words, scrab) { //takes words matching letter amount and matches individual letters
+  for(var i = 0; i < scrab.length; i++) {
+    if(scrab[i] !== "_") {
+      words = ac.indexes(words, scrab[i], i);
+    }
+  }//if theres a value in text box, pare down words using that letter
+  return words;
+};
+
+ac.indexes = function(arr, letter, index) {
+  // console.log('ac indexes', arr, letter, index);
+  return arr.filter(function(x) {
+    return x[index] === letter;
+  });
+};
 
 module.exports = ac;
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 var http = require('http');
 var port = process.env.PORT || 3000;
 var ac = require('./index.js');
+ac.import(function(err, count) {
+  console.log("Imported " + count.length + " Scrabble words!");
+});
+// update
 var fs = require('fs');
 var index = fs.readFileSync(__dirname + '/index.html');
 
 http.createServer(function handler(request, response) {
   var url = request.url;
-  console.log("request.url:", url);
   if (url.length === 1) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.end(index.toString());
   }
-  response.end('hello world!');
-
+  if (url.length > 1) {
+    var scrab = url.split('/')[1];
+    var ok = ac.numWords(scrab.length, scrab).join(',');
+    if(ok.length === 0) {response.end("No results found!")}
+    response.end(ok);
+  } else {
+    response.end('blank');
+  }
 }).listen(port);
 
 console.log('node http server listening on http://localhost:' + port);
